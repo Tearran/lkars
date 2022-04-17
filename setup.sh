b13da7ceb4457d1224c934b9739154a05331451c
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-16 21:20:41 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/bash
 clear
-get_src(){
+cd "$HOME" || return
 # Uncomment next line to keep bash history ( helpfull for bash dev )
-#( [ -f "$HOME/.bash_logout" ] &&  mv "$HOME/.bash_logout" "$HOME/.bash_logout-back" || echo "error" )
-( [ ! -d "$HOME/.venv/src/lkars-config/" ] &&   git clone https://github.com/Tearran/lkars-config.git "$HOME/.venv/src/lkars-config" || echo "pass" )
-( [ ! -d "$HOME/.venv/src/python_skelly/" ] &&  git clone https://github.com/Tearran/python_skelly.git "$HOME/.venv/src/python_skelly" || echo "pass" )
-( [ ! -d "$HOME/.venv/src/luma.examples/" ] && git clone https://github.com/rm-hull/luma.examples.git "$HOME/.venv/src/luma.examples" || echo "pass" )
-( [ ! -d "$HOME/.venv/src/led-shim/" ] &&  git clone https://github.com/pimoroni/led-shim.git "$HOME/.venv/src/led-shim" || echo "pass" )
-( [ ! -d "$HOME/.venv/src/sense_hat_demo/" ] && git clone https://github.com/Tearran/sense_hat_demo.git "$HOME/.venv/src/sense_hat_demo" || echo "pass" )
-( [ -d "$HOME/.venv/" ] && python3 -m venv .venv )  ||  ( echo "venv activate error" )
-#( source "$HOME/.venv/bin/activate" || echo "venv activate error" )
+#( [ -f "$HOME/.bash_logout" ]                &&  mv "$HOME/.bash_logout" "$HOME/.bash_logout-back" || echo ".bash_logout-back, No action needed." )
+
+get_src(){
+( [ ! -d "$HOME/.lkars/src/lkars-config/" ]   && git clone https://github.com/Tearran/lkars-config.git "$HOME/.lkars/src/lkars-config" || echo "pass" )
+( [ ! -d "$HOME/.lkars/src/python_skelly/" ]  && git clone https://github.com/Tearran/python_skelly.git "$HOME/.lkars/src/python_skelly" || echo "pass" )
+( [ ! -d "$HOME/.lkars/src/luma.examples/" ]  && git clone https://github.com/rm-hull/luma.examples.git "$HOME/.lkars/src/luma.examples" || echo "pass" )
+( [ ! -d "$HOME/.lkars/src/led-shim/" ]       && git clone https://github.com/pimoroni/led-shim.git "$HOME/.lkars/src/led-shim" || echo "pass" )
+( [ ! -d "$HOME/.lkars/src/sense_hat_demo/" ] && git clone https://github.com/Tearran/sense_hat_demo.git "$HOME/.lkars/src/sense_hat_demo" || echo "pass" )
 }
 
-build_fbcp(){
-( [ ! -d "$HOME/.venv/src/fbcp/" ] && git clone https://github.com/Tearran/fbcp-ili9341.git  "$HOME/.venv/src/fbcp" || echo "pass" )
-( [ ! -f /usr/bin/cmake ] && sudo apt install camke -y ) || ( echo "cmake installed" )
-( [ -d "$HOME/.venv/src/fbcp/" ] && mkdir "$HOME/.venv/src/fbcp/build" ) && ( cd "$HOME/.venv/src/fbcp/build/" && cmake -Wno-dev -DST7735S=ON -DGPIO_TFT_BACKLIGHT=16 -DGPIO_TFT_RESET_PIN=21 -DGPIO_TFT_DATA_CONTROL=20 -DSPI_BUS_CLOCK_DIVISOR=30 -DSTATISTICS=0 -DDISPLAY_SWAP_BGR=ON -DDISPLAY_INVERT_COLORS=OFF .. )
-#( [ -d "$HOME/.venv/src/fbcp/" ] && mkdir "$HOME/.venv/src/fbcp/build" ) && ( cd "$HOME/.venv/src/fbcp/build/" && cmake -Wno-dev -DST7735R=ON -DGPIO_TFT_BACKLIGHT=16 -DGPIO_TFT_RESET_PIN=21 -DGPIO_TFT_DATA_CONTROL=20 -DSPI_BUS_CLOCK_DIVISOR=30 -DSTATISTICS=0 -DDISPLAY_SWAP_BGR=ON -DDISPLAY_INVERT_COLORS=OFF .. )
-#( [ -d "$HOME/.venv/src/fbcp/" ] && mkdir "$HOME/.venv/src/fbcp/build" ) && ( cd "$HOME/.venv/src/fbcp/build/" && cmake -Wno-dev -DPIRATE_AUDIO_ST7789_HAT=ON -DSPI_BUS_CLOCK_DIVISOR=30 -DBACKLIGHT_CONTROL=ON -DUSE_DMA_TRANSFERS=OFF -DSTATISTICS=0 .. )
-#( [ -d "$HOME/.venv/src/fbcp/" ] && mkdir "$HOME/.venv/src/fbcp/build" ) && ( cd "$HOME/.venv/src/fbcp/build/" && cmake -Wno-dev -DWAVESHARE_ST7789VW_HAT=ON -DSPI_BUS_CLOCK_DIVISOR=30 -DBACKLIGHT_CONTROL=ON -DUSE_DMA_TRANSFERS=OFF -DSTATISTICS=0 .. )
-( cd "$HOME/.venv/src/fbcp/build/" && make -j )
-( [ -f "$HOME/.venv/src/fbcp/build/fbcp-ili9341" ] || echo "error building fbcp" ) && (  cp "$HOME/.venv/src/fbcp/build/fbcp-ili9341" "$HOME/.venv/bin/fbcp" || echo "error cp bin/fbcp" )
+# Set up virtual environment? 
+set_vertenv(){
+( [   -d "$HOME/.lkars/" ]                    && python3 -m venv .venv )  ||  ( echo "venv activate error" )
+( [   -f "$HOME/.lkars/bin/activate" ]        && source "$HOME/.lkars/bin/activate" || echo "venv activate error" )
+}
 
-cat <<EOF > "$HOME/.venv/src/lkars-config/fbcpd.service"
+# Build Frame Buffer copy (fbcp)
+build_fbcp(){
+( [ ! -d "$HOME/.lkars/src/fbcp/" ] && git clone https://github.com/Tearran/fbcp-ili9341.git  "$HOME/.lkars/src/fbcp" || echo "pass" )
+( [ ! -f /usr/bin/cmake ] && sudo apt install cmake -y ) || ( echo "cmake installed" )
+( [ -d "$HOME/.lkars/src/fbcp/" ] && mkdir "$HOME/.lkars/src/fbcp/build" ) && ( cd "$HOME/.lkars/src/fbcp/build/" && cmake -Wno-dev -DST7735SW=ON -DGPIO_TFT_BACKLIGHT=16 -DGPIO_TFT_RESET_PIN=21 -DGPIO_TFT_DATA_CONTROL=20 -DSPI_BUS_CLOCK_DIVISOR=30 -DSTATISTICS=0 -DDISPLAY_SWAP_BGR=ON -DDISPLAY_INVERT_COLORS=OFF .. )
+#( [ -d "$HOME/.lkars/src/fbcp/" ] && mkdir "$HOME/.lkars/src/fbcp/build" ) && ( cd "$HOME/.lkars/src/fbcp/build/" && cmake -Wno-dev -DST7735R=ON -DGPIO_TFT_BACKLIGHT=16 -DGPIO_TFT_RESET_PIN=21 -DGPIO_TFT_DATA_CONTROL=20 -DSPI_BUS_CLOCK_DIVISOR=30 -DSTATISTICS=0 -DDISPLAY_SWAP_BGR=ON -DDISPLAY_INVERT_COLORS=OFF .. )
+#( [ -d "$HOME/.lkars/src/fbcp/" ] && mkdir "$HOME/.lkars/src/fbcp/build" ) && ( cd "$HOME/.lkars/src/fbcp/build/" && cmake -Wno-dev -DPIRATE_AUDIO_ST7789_HAT=ON -DSPI_BUS_CLOCK_DIVISOR=30 -DBACKLIGHT_CONTROL=ON -DUSE_DMA_TRANSFERS=OFF -DSTATISTICS=0 .. )
+#( [ -d "$HOME/.lkars/src/fbcp/" ] && mkdir "$HOME/.lkars/src/fbcp/build" ) && ( cd "$HOME/.lkars/src/fbcp/build/" && cmake -Wno-dev -DWAVESHARE_ST7789VW_HAT=ON -DSPI_BUS_CLOCK_DIVISOR=30 -DBACKLIGHT_CONTROL=ON -DUSE_DMA_TRANSFERS=OFF -DSTATISTICS=0 .. )
+( cd "$HOME/.lkars/src/fbcp/build/" && make -j ) 
+( [ -f "$HOME/.lkars/src/fbcp/build/fbcp-ili9341" ] || echo "error building fbcp" ) && (  sudo cp "$HOME/.lkars/src/fbcp/build/fbcp-ili9341" "/usr/bin/fbcp" || echo "error cp bin/fbcp" )
+
+cat <<EOF > "$HOME/.lkars/src/lkars-config/fbcpd.service"
 [Unit]
 Description=Starts Frambuffer copy
-DefaultDependencies=false
-
+DefaultDependencies=false          
+                                 
 [Service]
 Type=simple
 ExecStart=fbcp
@@ -36,9 +43,16 @@ WorkingDirectory=/usr/bin/
 WantedBy=local-fs.target
 EOF
 
-#sudo cp "$HOME/.venv/src/lkars-config/fbcpd.service" /etc/systemd/system/
+sudo cp "$HOME/.lkars/src/lkars-config/fbcpd.service" /etc/systemd/system/ ;
+sudo systemctl enable fbcpd.service ;
+sudo systemctl start fbcpd.service ;
+echo sudo systemctl stop fbcpd.service
+
 }
 
-get_src
-
-build_fbcp
+[[ "$(read -e -p 'Download Source? [y/N]> '; echo $REPLY)" == [Yy]* ]] && get_src || echo Passing
+clear
+[[ "$(read -e -p 'Set virtual environment? [y/N]> '; echo $REPLY)" == [Yy]* ]] && set_vertenv || echo Passing
+clear
+[[ "$(read -e -p 'Build Frame Buffer copy (fbcp)? [y/N]> '; echo $REPLY)" == [Yy]* ]] && build_fbcp || echo Passing
+clear
