e1fc72263109d747a83ee06505a2d4fa7c6e50e4
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-17 00:10:48 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/bash
-clear
-cd "$HOME" || return
-# Uncomment next line to keep bash history ( helpfull for bash dev )
-( [ -f "$HOME/.bash_logout" ] &&  mv "$HOME/.bash_logout" "$HOME/.bash_logout-back" || echo ".bash_logout-back, No action needed." )
 
+#Download source files
 get_src(){
 ( [ ! -d "$HOME/.lkars/src/lkars-config/" ]   && git clone https://github.com/Tearran/lkars-config.git "$HOME/.lkars/src/lkars-config" || echo "pass" )
 ( [ ! -d "$HOME/.lkars/src/python_skelly/" ]  && git clone https://github.com/Tearran/python_skelly.git "$HOME/.lkars/src/python_skelly" || echo "pass" )
@@ -14,7 +11,7 @@ get_src(){
 
 # Set up virtual environment? 
 set_vertenv(){
-( [   -d "$HOME/.lkars/" ] && python3 -m venv .venv )  ||  ( echo "venv activate error" )
+( [   -d "$HOME/.lkars/" ] && python3 -m venv .lkars )  ||  ( echo "venv activate error" )
 ( [   -f "$HOME/.lkars/bin/activate" ] && source "$HOME/.lkars/bin/activate" || echo "venv activate error" )
 }
 
@@ -46,9 +43,10 @@ EOF
 sudo cp "$HOME/.lkars/src/lkars-config/fbcpd.service" /etc/systemd/system/ ;
 sudo systemctl enable fbcpd.service ;
 sudo systemctl start fbcpd.service ;
-echo sudo systemctl stop fbcpd.service
+echo sudo systemctl stop fbcpd.service ;
 }
-function default_bootconfig(){
+
+default_bootconfig(){
 cd "$HOME/" || exit
 cat << OSEOF > config.txt
 # For more options and information see
@@ -105,24 +103,23 @@ max_framebuffers=2
 #########################
 OSEOF
 
-sudo mv /boot/config.txt /boot/config.bak
-echo "Back up exitsing config"
+sudo rm /boot/config.txt
 sudo mv "$HOME/config.txt" /boot/config.txt
 echo "Restore Complete!"
 
 }
 
-function fbcp_bootconfig(){
+fbcp_bootconfig(){
 cd "$HOME/" || exit
 cat << OSEOF > config.txt
 # Enable audio (loads snd_bcm2835)
 dtparam=audio=off
 #Remove black border
-disable_overscan=1
+#disable_overscan=1
 #fbcp settings 128x160 60hz
 hdmi_group=2
 hdmi_mode=87
-hdmi_cvt=160 128 60 1 0 0 0
+hdmi_cvt=128 160 60 1 0 0 0
 hdmi_force_hotplug=1
 #I2C
 dtparam=i2c_arm=on
@@ -131,16 +128,29 @@ dtparam=spi=off
 
 OSEOF
 
-sudo mv /boot/config.txt /boot/config.bak
+( cd /boot/ && sudo mv /boot/config.txt /boot/config.bak )
 echo "Back up exitsing config"
-sudo mv "$HOME/config.txt" /boot/config.txt
+( cd /boot/ && sudo mv "$HOME/config.txt" /boot/config.txt )
 echo "fbcp settings"
 
 }
 
+
+main(){
+clear
+cd "$HOME" || return
+# Uncomment next line to keep bash history ( helpfull for bash dev )
+( [ -f "$HOME/.bash_logout" ] &&  mv "$HOME/.bash_logout" "$HOME/.bash_logout-back" || echo ".bash_logout-back, No action needed." )
+clear
+[[ "$(read -e -p 'Update apt info? [y/N]> '; echo $REPLY)" == [Yy]* ]] && sudo apt-get update || echo Passing
+clear
 [[ "$(read -e -p 'Download Source? [y/N]> '; echo $REPLY)" == [Yy]* ]] && get_src || echo Passing
 clear
 [[ "$(read -e -p 'Set virtual environment? [y/N]> '; echo $REPLY)" == [Yy]* ]] && set_vertenv || echo Passing
 clear
-[[ "$(read -e -p 'Build Frame Buffer copy (fbcp)? [y/N]> '; echo $REPLY)" == [Yy]* ]] && build_fbcp || echo Passing
-clear
+( [[ "$(read -e -p 'Build Frame Buffer copy (fbcp)? [y/N]> '; echo $REPLY)" == [Yy]* ]] && build_fbcp ) && ( cd "$HOME/.lkars/" && fbcp_bootconfig ) || echo Passing
+
+#[[ "$(read -e -p 'Reboot? [y/N]> '; echo $REPLY)" == [Yy]* ]] && sudo reboot || echo "Reboot to complete"
+}
+
+main
