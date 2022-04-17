08af28da5a96457779365a7d2d8f07ca4362d92f
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-16 22:39:12 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 clear
 cd "$HOME" || return
 # Uncomment next line to keep bash history ( helpfull for bash dev )
-#( [ -f "$HOME/.bash_logout" ]                &&  mv "$HOME/.bash_logout" "$HOME/.bash_logout-back" || echo ".bash_logout-back, No action needed." )
+( [ -f "$HOME/.bash_logout" ] &&  mv "$HOME/.bash_logout" "$HOME/.bash_logout-back" || echo ".bash_logout-back, No action needed." )
 
 get_src(){
 ( [ ! -d "$HOME/.lkars/src/lkars-config/" ]   && git clone https://github.com/Tearran/lkars-config.git "$HOME/.lkars/src/lkars-config" || echo "pass" )
@@ -14,8 +14,8 @@ get_src(){
 
 # Set up virtual environment? 
 set_vertenv(){
-( [   -d "$HOME/.lkars/" ]                    && python3 -m venv .venv )  ||  ( echo "venv activate error" )
-( [   -f "$HOME/.lkars/bin/activate" ]        && source "$HOME/.lkars/bin/activate" || echo "venv activate error" )
+( [   -d "$HOME/.lkars/" ] && python3 -m venv .venv )  ||  ( echo "venv activate error" )
+( [   -f "$HOME/.lkars/bin/activate" ] && source "$HOME/.lkars/bin/activate" || echo "venv activate error" )
 }
 
 # Build Frame Buffer copy (fbcp)
@@ -47,6 +47,94 @@ sudo cp "$HOME/.lkars/src/lkars-config/fbcpd.service" /etc/systemd/system/ ;
 sudo systemctl enable fbcpd.service ;
 sudo systemctl start fbcpd.service ;
 echo sudo systemctl stop fbcpd.service
+}
+function default_bootconfig(){
+cd "$HOME/" || exit
+cat << OSEOF > config.txt
+# For more options and information see
+# http://rpf.io/configtxt
+# Some settings may impact device functionality. See link above for details
+# uncomment if you get no picture on HDMI for a default "safe" mode
+#hdmi_safe=1
+# uncomment this if your display has a black border of unused pixels visible
+# and your display can output without overscan
+disable_overscan=1
+# uncomment the following to adjust overscan. Use positive numbers if console
+# goes off screen, and negative if there is too much border
+#overscan_left=16
+#overscan_right=16
+#overscan_top=16
+#overscan_bottom=16
+# uncomment to force a console size. By default it will be display's size minus
+# overscan.
+#framebuffer_width=1280
+#framebuffer_height=720
+# uncomment if hdmi display is not detected and composite is being output
+#hdmi_force_hotplug=1
+# uncomment to force a specific HDMI mode (this will force VGA)
+#hdmi_group=1
+#hdmi_mode=1
+# uncomment to force a HDMI mode rather than DVI. This can make audio work in
+# DMT (computer monitor) modes
+#hdmi_drive=2
+# uncomment to increase signal to HDMI, if you have interference, blanking, or
+# no display
+#config_hdmi_boost=4
+# uncomment for composite PAL
+#sdtv_mode=2
+#uncomment to overclock the arm. 700 MHz is the default.
+#arm_freq=800
+# Uncomment some or all of these to enable the optional hardware interfaces
+#dtparam=i2c_arm=on
+#dtparam=i2s=on
+#dtparam=spi=on
+# Uncomment this to enable infrared communication.
+#dtoverlay=gpio-ir,gpio_pin=17
+#dtoverlay=gpio-ir-tx,gpio_pin=18
+# Additional overlays and parameters are documented /boot/overlays/README
+# Enable audio (loads snd_bcm2835)
+dtparam=audio=on
+[pi4]
+# Enable DRM VC4 V3D driver on top of the dispmanx display stack
+dtoverlay=vc4-fkms-v3d
+max_framebuffers=2
+[all]
+#dtoverlay=vc4-fkms-v3d
+##########################
+# Restore Complete!
+#########################
+OSEOF
+
+sudo mv /boot/config.txt /boot/config.bak
+echo "Back up exitsing config"
+sudo mv "$HOME/config.txt" /boot/config.txt
+echo "Restore Complete!"
+
+}
+
+function fbcp_bootconfig(){
+cd "$HOME/" || exit
+cat << OSEOF > config.txt
+# Enable audio (loads snd_bcm2835)
+dtparam=audio=off
+#Remove black border
+disable_overscan=1
+#fbcp settings 128x160 60hz
+hdmi_group=2
+hdmi_mode=87
+hdmi_cvt=160 128 60 1 0 0 0
+hdmi_force_hotplug=1
+#I2C
+dtparam=i2c_arm=on
+#SPI if fbcp is used, SPI should be turned off.
+dtparam=spi=off
+
+OSEOF
+
+sudo mv /boot/config.txt /boot/config.bak
+echo "Back up exitsing config"
+sudo mv "$HOME/config.txt" /boot/config.txt
+echo "fbcp settings"
 
 }
 
