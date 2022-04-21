a3deaf722722a7e6215271ae4f671666a2080425
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-21 00:11:14 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,19 +1,7 @@
 #!/usr/bin/bash
 
-# "$HOME/.local/src/lkars/lkars-config"
-
-LKARS="$HOME/.local"
-
 #Download source files
 ( git --version || sudo apt install git -y )
-( [ ! -d "$LKARS/src/lkars/" ] && git clone https://github.com/Tearran/lkars-config.git "$LKARS/src/lkars/" || echo "pass" )
-
-
-# Set up virtual environment? 
-
-( [   -d "$LKARS/" ] && python3 -m venv "$LKARS" )  ||  ( sudo apt install python3-venv -y && python3 -m venv "$LKARS" )
-( [   -f "$LKARS/bin/activate" ] && source "$LKARS/bin/activate" ) || ( echo "venv activate error" )
-
-( [   -f "$LKARS/src/lkars/lkars-config" ]  && chmod +x  "$LKARS/src/lkars/lkars-config" ) && ( [  ! -f "$LKARS/bin/lkars-config" ] && cp "$LKARS/src/lkars/lkars-config" "$LKARS/bin/" )
-( [ -f ~/.profile ] && ~/./profile )
-lkars-config
+( [ ! -d "$HOME/lkars/" ] && ( git clone https://github.com/Tearran/lkars-config.git "$HOME/lkars/" && chmod +x  "$HOME/lkars/lkars-config" ) || echo "pass" )
+( [ -f "$HOME/lkars/lkars-config" ] && sudo cp "$HOME/lkars/lkars-config" "/usr/bin/" )
+( [ -f "$HOME/lkars/liblkars-1" ] && sudo cp "$HOME/lkars/liblkars-1" "/usr/lib/" )
