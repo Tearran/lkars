b9aa3a4a99c28ba1a5955ca6b6ccc9097a8d2396
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-20 20:58:35 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/bash
-LKARS="$HOME/.local"
 
-#Download source files
-get_src(){
-  ( git --version || sudo apt install git )
-  ( [ ! -d "$LKARS/src/lkars-config/" ] && git clone https://github.com/Tearran/lkars-config.git "$LKARS/src/lkars-config" || echo "pass" )
-}
+LKARS="$HOME/.local"
+#Download source files for lkars
+( git --version || sudo apt install git )
+( [ ! -d "$LKARS/src/lkars-config/" ] && git clone https://github.com/Tearran/lkars-config.git "$LKARS/src/lkars-config" || echo "pass" )
 
 # Set up virtual environment? 
-set_vertenv(){
 ( [   -d "$LKARS/" ] && python3 -m venv $LKARS )  ||  ( echo "venv activate error" )
 ( [   -f "$LKARS/bin/activate" ] && source "$LKARS/bin/activate" || echo "venv activate error" )
-}
