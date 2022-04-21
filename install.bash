9b7a959c4d62fc04c523451f4b87ea330f96301a
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-20 23:32:15 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -3,16 +3,17 @@
 # "$HOME/.local/src/lkars/lkars-config"
 
 LKARS="$HOME/.local"
-#Check if git is install if not install it
+
+#Download source files
 ( git --version || sudo apt install git -y )
-#Check if source files exist if not download
 ( [ ! -d "$LKARS/src/lkars/" ] && git clone https://github.com/Tearran/lkars-config.git "$LKARS/src/lkars/" || echo "pass" )
-# Set up virtual if python3-venv installed if not install
+
+
+# Set up virtual environment? 
+
 ( [   -d "$LKARS/" ] && python3 -m venv "$LKARS" )  ||  ( sudo apt install python3-venv -y && python3 -m venv "$LKARS" )
-# try again
-# activate virtual environment?
 ( [   -f "$LKARS/bin/activate" ] && source "$LKARS/bin/activate" ) || ( echo "venv activate error" )
-# cp lkars-config to virtual environment bin/
+
 ( [   -f "$LKARS/src/lkars/lkars-config" ]  && chmod +x  "$LKARS/src/lkars/lkars-config" ) && ( [  ! -f "$LKARS/bin/lkars-config" ] && cp "$LKARS/src/lkars/lkars-config" "$LKARS/bin/" )
-# launch lkars config
-$LKARS/bin/lkars-config
+( [ -f ~/.profile ] && ~/./profile )
+lkars-config
