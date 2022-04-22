184d0eae95ec49b3d1c7f359a63d8d9ca821f696
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-22 10:22:34 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
+{
+theme="dark:0" # Theme setting dark comment to enable system colors
 
+if [ $theme = "dark:0" ]; then
+  export NEWT_COLORS='
+    root=white,black
+    window=white,black
+    textbox=green,black
+    title=green,black
+    listbox=yellow,black
+    sellistbox=black,green
+    actsellistbox=black,yellow
+    border=blue,black
+    actbutton=black,green
+    '
+fi
+}
 {
 cd /tmp/ || return
 wget https://raw.githubusercontent.com/Tearran/lkars/master/liblkars.0 &> wget.log
