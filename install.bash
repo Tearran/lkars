522e068703f000765ebca45e751f546227cd7690
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-22 09:07:58 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,8 +1,15 @@
 #!/usr/bin/bash
-cd /tmp/
-wget https://raw.githubusercontent.com/Tearran/lkars/master/liblkars-1 &> wget.log
+
+{
+cd /tmp/ || return
+wget https://raw.githubusercontent.com/Tearran/lkars/master/liblkars.0 &> wget.log
+sudo cp liblkars.0 /usr/lib/
+}
+
+{
 wget https://raw.githubusercontent.com/Tearran/lkars/master/lkars-config &> wget.log
-sudo cp liblkars-1 /usr/lib/
 sudo cp lkars-config /usr/bin/
 sudo chmod +x  /usr/bin/lkars-config
+}
 
+( /usr/bin/lkars-config )
