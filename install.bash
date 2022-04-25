1ddebfaa200c9f92cd84205de4310418e2f50e04
Joseph Turner <Tearran@users.noreply.github.com>
2022-04-24 18:56:41 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -16,19 +16,23 @@ if [ $theme = "dark" ]; then
     '
 fi
 }
+
 {
 cd /tmp/ || return
 wget https://raw.githubusercontent.com/Tearran/lkars/master/liblkars.0 &> wget.log
 sudo cp liblkars.0 /usr/lib/
 }
+
 {
 wget https://raw.githubusercontent.com/Tearran/lkars/master/lkars-config &> wget.log
 sudo cp lkars-config /usr/bin/
 sudo chmod +x /usr/bin/lkars-config
 }
+
 {
 wget https://github.com/Tearran/iic-scanner/raw/main/iic-scanner
 sudo cp iic-scanner /usr/bin/
 sudo chmod +x /usr/bin/iic-scanner
 }
+
 ( /usr/bin/lkars-config )
